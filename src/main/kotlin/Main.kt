f2f89358b634ca71d7dea3588fa81d7a344c237d
m.badrmiran <masoud.badremiran@gmail.com>
2022-06-15 10:18:44 +0430

    Add peek,count and isEmpty

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val stack = Stack<Int>().apply {
+        push(1)
+        push(2)
+        push(3)
+    }
+
+    print(stack)
+
 }
--- src/main/kotlin/Stack.kt
+++ src/main/kotlin/Stack.kt
@@ -1,4 +1,29 @@
-interface Stack<Element> {
-    fun push(element: Element)
-    fun pop() : Element?
+class Stack<T> {
+    private val storage = arrayListOf<T>()
+
+    val count : Int
+    get() = storage.size
+
+    val isEmpty : Boolean
+    get() = count == 0
+
+    override fun toString() = buildString {
+        appendLine("---top---")
+        storage.asReversed().forEach {
+            appendLine("$it")
+        }
+        appendLine("---end---")
+    }
+
+    fun push(element: T) = storage.add(element)
+
+    fun pop(): T? {
+        if (storage.size == 0)
+            return null
+
+        return storage.removeAt(storage.size - 1)
+    }
+
+    fun peek() : T? = storage.lastOrNull()
+
 }
